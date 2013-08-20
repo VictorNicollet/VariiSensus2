33c2f451699a510354d26a28e8dc79dd589b56a0
Victor Nicollet <victor@nicollet.net>
2013-08-20 12:11:36 +0200

    Exploration

--- all.ml
+++ all.ml
@@ -9,6 +9,7 @@
 "esclaves" ;           (* Nathan rencontre Orona à Virnalvir. *)
 "ordo-aureus" ;        (* Staniel exécute Akim et nomme Hans Banach à sa 
 			  place. *)
+"exploration" ;        (* Onterberg et Lucrèce arrivent au fond du monde. *)
 "dernier-temple" ;     (* Simmera arrive au Dernier Temple. *)
 "triangle" ;           (* Staniel, Julie et Simmera parlent de leurs 
 			  projets. *)
