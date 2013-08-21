665a2b04d4220e3e25c4bba8b9085426eea38956
Victor Nicollet <victor@nicollet.net>
2013-08-21 14:29:17 +0200

    Fusées

--- all.ml
+++ all.ml
@@ -6,13 +6,20 @@
 "presses" ;            (* Saïar parle à Nathan des actions d'Orona à Hayako. *)
 "la-meute" ;           (* Galilée vient se plaindre à Akim des filles du 
 		          cauchemar. *)
-"transaction" ;        (* Nathan rencontre Masque, lui vend des fusées, 
+"transaction" ;        (* Nathan rencontre Carnaval, lui vend des fusées, 
 			  récupère une commande et une gorgone. *)
+"fusees" ;             (* Le Tyran Boréal est détruit. *)
 "naufrage" ;           (* Simmera atterrit en rase campagne. *)
+
+(* Akim rencontre la meute, parle de Orona. *)
+
 "esclaves" ;           (* Nathan rencontre Orona à Virnalvir. *)
 "ordo-aureus" ;        (* Staniel exécute Akim et nomme Hans Banach à sa 
 			  place. *)
 "exploration" ;        (* Onterberg et Lucrèce arrivent au fond du monde. *)
+
+(* Simmera et Lugoj affrontent les esprits. *)
+
 "dernier-temple" ;     (* Simmera arrive au Dernier Temple. *)
 "triangle" ;           (* Staniel, Julie et Simmera parlent de leurs 
 			  projets. *)
